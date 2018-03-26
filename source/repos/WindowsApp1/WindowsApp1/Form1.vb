dc00a1dfa55b881da6cf4c72222db90960a2d40d
camilaViquez <32073514+camilaViquez@users.noreply.github.com>
2018-03-25 23:02:23 -0600

    First commit

---
+++ source/repos/WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Integer
+        a = 5
+        Dim b As String
+        b = TextBox1.Text()
+
+
+
+        a = CInt(b)
+        Console.WriteLine(a)
+
+
+
+
+    End Sub
+End Class
